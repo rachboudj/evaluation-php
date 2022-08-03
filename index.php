916271ac4ac5f99a52ae4ec7611980adc5749e06
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-03 20:08:08 +0200

    ajout d'une condition pour mettre le header d'un administrateur si l'utilisateur a pour rôle administrateur

--- index.php
+++ index.php
@@ -6,3 +6,8 @@ autoLoad("*.php");
 require_once './includes/header.php';
 require_once './includes/main.php';
 require_once './includes/footer.php';
+
+if (verifierAdmin()) 
+    require_once './includes/headerAdmin.php';
+else 
+    require_once './includes/header.php';
