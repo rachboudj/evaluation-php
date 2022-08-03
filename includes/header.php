92ca0ca6c93f1969a1d8b545e1b3d028944068ae
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-03 20:00:20 +0200

    ajout d'un lien pour pouvoir se connecter ou se deconnecter

--- includes/header.php
+++ includes/header.php
@@ -17,6 +17,13 @@
                 <li><a href="index.php?page=accueil">Accueil</a></li>
                 <li><a href="index.php?page=inscription">Inscription</a></li>
                 <li><a href="index.php?page=contact">Contact</a></li>
+                <?php
+                if (isset($_SESSION['login']) && $_SESSION['login'] === true) {
+                    echo '<li><a href="index.php?page=logout">Logout</a></li>';
+                } else {
+                    echo '<li><a href="index.php?page=login">Login</a></li>';                    
+                }
+                ?>
             </ul>
         </nav>
     </header>
