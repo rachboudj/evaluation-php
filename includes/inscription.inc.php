ea88bdbcbbb0cd8c283b0ce44b02b87fb3a37b39
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-03 19:21:57 +0200

    déplacement du formulaire vers un fichier à part

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -1,6 +1,7 @@
+<h1>Inscription</h1>
 <?php 
 
-if (isset($_POST['inscription'])) {
+if (isset($_POST['frmInscription'])) {
     $nom = isset($_POST['nom']) ? htmlentities(trim($_POST['nom'])) : "";
     $prenom = isset($_POST['prenom']) ? htmlentities(trim($_POST['prenom'])) : "";
     $email = isset($_POST['email']) ? htmlentities(trim($_POST['email'])) : "";
@@ -56,46 +57,11 @@ if (isset($_POST['inscription'])) {
                 $message = "Erreur";
 
             echo $message;
-
-            echo "<script>window.location.replace('http://localhost:8886/evaluation-php-rachid?index.php=accueil')</script>";
         }
     }
 } else {
     $nom = $prenom = $email = $cgu = "";
-    require_once './includes/inscription.inc.php';
+    require_once './includes/frmInscription.php';
 }
 
 ?>
-
-<h1>Inscription</h1>
-
-<form action="index.php?page=inscription" method="post">
-    <div>
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" value="<?=$nom?>" />
-    </div>
-    <div>
-        <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" value="<?=$prenom?>" />
-    </div>
-    <div>
-        <label for="email">E-mail :</label>
-        <input type="text" id="email" name="email" value="<?=$email?>" />
-    </div>
-    <div>
-        <label for="mdp1">Mot de passe :</label>
-        <input type="password" id="mdp1" name="mdp1" />
-    </div>
-    <div>
-        <label for="mdp2">Vérification mot de passe :</label>
-        <input type="password" id="mdp2" name="mdp2" />
-    </div>
-    <div>
-        <input type="checkbox" name="cgu" id="cgu" value="1"<?=isset($_POST['cgu'])?"checked":'';?> /><label for="cgu" >J'accepte les <a href="index.php?page=cgu" target="_blank">Conditions Générales d'Utilisation</a></label>
-    </div>
-    <div>
-        <input type="reset" value="Effacer" />
-        <input type="submit" value="Envoyer" />
-    </div>
-    <input type="hidden" name="inscription" />
-</form>
