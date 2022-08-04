4eb73537295bc9b59efc4af5601e1de826118509
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-05 00:16:34 +0200

    affichage des détails de l'article dans l'html

--- includes/detailArticle.inc.php
+++ includes/detailArticle.inc.php
@@ -1,3 +1,4 @@
+<h1>Détails de l'article</h1>
 <?php 
 
 if(!empty($_GET['id']) && is_numeric($_GET['id'])) 
@@ -17,3 +18,8 @@ if(!empty($_GET['id']) && is_numeric($_GET['id']))
 
 ?>
 
+<div class="container">
+    <h2><?php echo ucfirst($article['titre']); ?></h2>
+    <p><?php echo nl2br($article['description']); ?></p>
+    <p>Date: <?php echo date('d/m/Y à H:i:s', strtotime($article['created_at'])); ?></p>
+</div>
