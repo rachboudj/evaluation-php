02e6a7af3c4521bfd3d32d5c4e0125efe82d9970
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-04 14:08:04 +0200

    corrections mineures

--- functions/validationTexte.php
+++ functions/validationTexte.php
@@ -4,12 +4,12 @@ function validationTexte($er, $data, $key, $min, $max)
 {
     if(!empty($data)) {
         if(mb_strlen($data) < $min) {
-            $er[$key] = 'min '.$min.' caractères';
+            $er[$key] = 'Il faut minimum '.$min.' caractères.';
         } elseif(mb_strlen($data) >= $max) {
-            $er[$key] = 'max '.$max.' caractères';
+            $er[$key] = 'Il faut maximum '.$max.' caractères.';
         }
     } else{
-        $er[$key] = 'Veuillez renseigner ce champ';
+        $er[$key] = 'Veuillez renseigner ce champ.';
     }
     return $er;
 }
