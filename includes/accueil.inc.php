676870e25eca8f012321d0fa464f3692de540790
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-04 17:38:45 +0200

    ajout de la requête qui permet d'afficher les articles publié par ordre de création

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,1 +1,12 @@
 <h1>Accueil</h1>
+
+<?php 
+
+$sql = "SELECT * FROM articles WHERE status = 'publish' ORDER BY created_at LIMIT 10";
+$query = pdo()->prepare($sql);
+$query->execute();
+$articles = $query->fetchAll();
+
+?>
+
+
