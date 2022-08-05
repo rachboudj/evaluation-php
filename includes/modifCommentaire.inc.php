f54d4ca4fb9868c427b9d6a25cc30ca6ef53ac60
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-05 23:19:22 +0200

    modification de la variable dans les inputs et suppression de la variable status car valeur par défaut

--- includes/modifCommentaire.inc.php
+++ includes/modifCommentaire.inc.php
@@ -15,11 +15,9 @@ if(!empty($_GET['articleId']) && is_numeric($_GET['articleId'])) {
     {
         $description = trim(strip_tags($_POST['description']));
         $auteur = trim(strip_tags($_POST['auteur']));
-        $status = trim(strip_tags($_POST['status']));
 
         $errors = validationTexte($errors,$description,'description',10,1000);
         $errors = validationTexte($errors,$auteur,'auteur',3,100);
-        $errors = validationTexte($errors,$status,'status',3,100);
 
         if(count($errors) === 0) 
         {
@@ -27,7 +25,6 @@ if(!empty($_GET['articleId']) && is_numeric($_GET['articleId'])) {
             $query = $pdo=pdo()->prepare($sql);
             $query->bindValue(':description',$description, PDO::PARAM_STR);
             $query->bindValue(':auteur',$auteur, PDO::PARAM_STR);
-            $query->bindValue(':status',$status, PDO::PARAM_STR);
             $query->bindValue(':id_commentaire',$id, PDO::PARAM_INT);
             $query->execute();
             // echo "<script>window.location.replace('http://localhost:8886/evaluation-php-rachid/index.php?page=articlesAdmin')</script>";
@@ -42,11 +39,11 @@ if(!empty($_GET['articleId']) && is_numeric($_GET['articleId'])) {
 <form action="" method="post">
 
     <label for="description">Contenu</label>
-    <textarea name="description" id="description" cols="30" rows="10"><?= getValue('description', $article['description']) ?></textarea>
+    <textarea name="description" id="description" cols="30" rows="10"><?= getValue('description', $commentaire['description']) ?></textarea>
     <span class="error"><?php if (!empty($errors['description'])) {echo $errors['description'];} ?></span>
 
     <label for="auteur">Auteur</label>
-    <input type="text" name="auteur" id="auteur" value="<?= getValue('auteur', $article['auteur']) ?>">
+    <input type="text" name="auteur" id="auteur" value="<?= getValue('auteur', $commentaire['auteur']) ?>">
     <span class="error"><?php if (!empty($errors['auteur'])) {echo $errors['auteur'];} ?></span>
 
     <input type="submit" name="submitted" value="Modifier le commentaire">
