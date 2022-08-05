9e33a345623137d9612242eec347b124791762fa
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-05 15:05:49 +0200

    ajout de la requête pour afficher le commentaire après le déatil de l'article, et ajout des commentaires de l'articles après l'article

--- includes/detailArticle.inc.php
+++ includes/detailArticle.inc.php
@@ -16,6 +16,12 @@ if(!empty($_GET['id']) && is_numeric($_GET['id']))
     die('Erreur 404');
 }
 
+$sql = "SELECT * FROM commentaires WHERE id_article = :id_article";
+$query = $pdo=pdo()->prepare($sql);
+$query->bindValue(':id_article',$id,PDO::PARAM_INT);
+$query->execute();
+$commentaires = $query->fetchAll();
+
 $errors = [];
 if(!empty($_POST['submitted'])) {
     $auteur = trim(strip_tags($_POST['auteur']));
@@ -23,14 +29,14 @@ if(!empty($_POST['submitted'])) {
     $errors = validationTexte($errors,$auteur,'auteur',3, 40);
     $errors = validationTexte($errors,$description,'description',3,2000);
     if(count($errors) == 0) {
-        $sql = "INSERT INTO commentaires (id_commentaire,description, auteur, created_at,modified_at,status)
-            VALUES (:id_commentaire,:description,:auteur,NOW(),NOW(),'Nouveau')";
+        $sql = "INSERT INTO commentaires (id_article,description, auteur, created_at,modified_at,status)
+            VALUES (:id_article,:description,:auteur,NOW(),NOW(),'new')";
         $query = $pdo=pdo()->prepare($sql);
         $query->bindValue(':auteur',$auteur,PDO::PARAM_STR);
         $query->bindValue(':description',$description,PDO::PARAM_STR);
-        $query->bindValue(':id_commentaire',$id,PDO::PARAM_INT);
+        $query->bindValue(':id_article',$id,PDO::PARAM_INT);
         $query->execute();
-        //echo "<script>window.location.replace('http://localhost:8886/evaluation-php-rachid/index.php?page=detailArticle&id=4')</script>";
+        // exit();
     }
 }
 
@@ -53,4 +59,14 @@ if(!empty($_POST['submitted'])) {
 
         <input type="submit" name="submitted" value="Ajouter">
     </form>
+
+    <?php if(!empty($commentaires)) { ?>
+        <h2>Les commentaires</h2>
+        <?php foreach ($commentaires as $commentaire) { ?>
+            <div class="commentaires">
+                <p>Auteur : <?= $commentaire['auteur']?></p>
+                <p>Content : <?= $commentaire['description']?></p>
+            </div>
+        <?php } ?>
+    <?php } ?>
 </div>
