ab518352301398d3ccbc3861299de6b6bfafd726
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-06 02:20:23 +0200

    modif du nom de la fonction pour vérifier qu'un utilisateur est un utilisateur inscrit

--- functions/verifierUserInscrit.php
+++ functions/verifierUserInscrit.php
@@ -1,6 +1,6 @@
 <?php
 
-function verifierAdmin(): bool {
+function verifierUserInscrit(): bool {
     if (isset($_SESSION['login']) && $_SESSION['login'] === true && $_SESSION['role'] === 'utilisateurInscrit') 
         return true;
     else
