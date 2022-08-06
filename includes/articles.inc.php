7774ed7dea7a67ec11f70b4e6a1e68013cad967d
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-06 02:34:10 +0200

    affichage des articles sur la page

--- includes/articles.inc.php
+++ includes/articles.inc.php
@@ -8,3 +8,25 @@ $query->execute();
 $articles = $query->fetchAll();
 
 ?>
+
+<section id="articles">
+    <hr>
+    <h2>Quelques articles</h2>
+    <p class="title">Mes derniers articles !</p>
+    <div class="container-article">
+        <?php foreach ($articles as $article) {
+        ?>
+            <div class="card-article">
+                <div class="text-card">
+                    <p class="status"><?= ucfirst($article['status']); ?></p>
+                    <h3><?= ucfirst($article['titre']); ?></h3>
+                    <p><?= ucfirst($article['auteur']); ?></p>
+                    <button><a href="index.php?page=detailArticle&amp;id=<?= $article['id_article']; ?>">Voir plus</a></button>
+                </div>
+
+            </div>
+
+        <?php } ?>
+    </div>
+
+</section>
