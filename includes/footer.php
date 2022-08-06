5569aadc42613ad2dbf8d59f986770fcfe9cdc83
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-06 03:21:48 +0200

    ajout lien vers page mentions légales

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
 <footer>
+    <a href="index.php?page=mentionsLégales" target="_blank">Mentions légales</a>
     <p class="copy">&copy; <?php echo date('Y'); ?> - Évaluation de PHP</p>
 </footer>   
 </body>
