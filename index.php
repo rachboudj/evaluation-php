1e73033e483e11f3614fe988f8821828f2c21421
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-06 03:17:17 +0200

    ajout d'accolade

--- index.php
+++ index.php
@@ -5,14 +5,25 @@ session_start();
 require_once './functions/autoLoad.php';
 autoLoad("*.php");
 
-if (verifierAdmin()) 
+if (verifierAdmin()) {
     require_once './includes/headerAdmin.php';
-if (verifierModerateur()) 
-    require_once './includes/headerModerateur.php';
-if (verifierRedacteur()) 
-    require_once './includes/headerRedacteur.php';
-else 
+} else {
     require_once './includes/header.php';
+}
+
+if (verifierModerateur()) 
+{
+    require_once './includes/headerModerateur.php';
+} else {
+    require_once './includes/header.php';
+}
+
+if (verifierRedacteur()) {
+    require_once './includes/headerRedacteur.php';
+} else {
+    require_once './includes/header.php';
+}
+
 
 
 
