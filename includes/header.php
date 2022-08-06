289c750c76abde122518129004838efc4d554eec
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-06 02:38:11 +0200

    ajout lien vers tous les articles

--- includes/header.php
+++ includes/header.php
@@ -15,6 +15,7 @@
         <nav>
             <ul>
                 <li><a href="index.php?page=accueil">Accueil</a></li>
+                <li><a href="index.php?page=articles">Tous les articles</a></li>
                 <li><a href="index.php?page=inscription">Inscription</a></li>
                 <li><a href="index.php?page=contact">Contact</a></li>
                 <?php
