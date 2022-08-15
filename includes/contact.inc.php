5a7121514976609e1434de0465bfb2c09234578a
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-08-15 16:44:05 +0200

    ajout d'un formulaire pour la page contact

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,1 +1,12 @@
 <h1>Contact</h1>
+
+<form action="index.php?page=contact">
+    <label for="prenom">Prénom</label>
+    <input type="text" name="prenom" id="prenom">
+    <label for="nom">Nom</label>
+    <input type="text" name="nom" id="nom">
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email">
+    <label for="commentaire">Commentaire</label>
+    <input type="text" name="commentaire" id="commentaire">
+</form>
